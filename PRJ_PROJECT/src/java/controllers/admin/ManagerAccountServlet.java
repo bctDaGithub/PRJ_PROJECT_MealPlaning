deb9c270c75c7d5a8974d7d992b8a80414eb218f
notKaaaa <khabuianh@gmail.com>
2024-07-16 19:55:14 +0700

    Add files via upload

---
+++ PRJ_PROJECT/src/java/controllers/admin/ManagerAccountServlet.java
@@ -0,0 +1,43 @@
+package controllers.admin;
+
+import dao.UserDAO;
+import dto.Account;
+import java.io.IOException;
+import java.util.List;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@WebServlet(name = "ManagerAccountServlet", urlPatterns = {"/managerAccount"})
+public class ManagerAccountServlet extends HttpServlet {
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        request.setCharacterEncoding("UTF-8");
+        String txtSearch = request.getParameter("valueSearch");
+        UserDAO dao = new UserDAO();
+        List<Account> list = dao.getAllUsers();
+        request.setAttribute("listUser", list);
+        request.getRequestDispatcher("managerUser.jsp").forward(request, response);
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }
+}
